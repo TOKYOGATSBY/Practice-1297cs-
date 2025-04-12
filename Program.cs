db45cb136c2b7b967f5418461292188c2380c60b
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-04-12 08:28:20 +0300

    version 1.0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+
+        int n = s.Length;
+        if (n == 0)
+        {
+            Console.WriteLine("");
+            return;
+        }
+
+        int maxLen = 1;
+        int startIndex = 0;
+
+        for (int center = 0; center < n; ++center)
+        {
+            {
+                int left = center, right = center;
+                while (left >= 0 && right < n && s[left] == s[right])
+                {
+                    int length = right - left + 1;
+                    if (length > maxLen)
+                    {
+                        maxLen = length;
+                        startIndex = left;
+                    }
+                    left--;
+                    right++;
+                }
+            }
+
+            {
+                int left = center, right = center + 1;
+                while (left >= 0 && right < n && s[left] == s[right])
+                {
+                    int length = right - left + 1;
+                    if (length > maxLen)
+                    {
+                        maxLen = length;
+                        startIndex = left;
+                    }
+                    left--;
+                    right++;
+                }
+            }
+        }
+
+        Console.WriteLine(s.Substring(startIndex, maxLen));
+    }
+}
